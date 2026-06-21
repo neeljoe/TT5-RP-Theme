30b41bf19b98aa1b0ae00629af2f791a30c278d7
neeljoe <joe.neel@gmail.com>
2026-06-21 22:29:15 +0530

    ref removed

--- patterns/header.php
+++ patterns/header.php
@@ -19,13 +19,13 @@
 <div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:site-title {"level":0} /-->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"right"}} -->
-<div class="wp-block-group"><!-- wp:navigation {"ref":53,"overlayBackgroundColor":"base","overlayTextColor":"contrast","metadata":{"blockVisibility":{"viewport":{"tablet":false,"mobile":false}}},"layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"}} /--></div>
+<div class="wp-block-group"><!-- wp:navigation {"overlayBackgroundColor":"base","overlayTextColor":"contrast","metadata":{"blockVisibility":{"viewport":{"tablet":false,"mobile":false}}},"layout":{"type":"flex","justifyContent":"right","flexWrap":"wrap"}} /--></div>
 <!-- /wp:group -->
 
 <!-- wp:group {"style":{"spacing":{"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group"><!-- wp:icon {"icon":"core/search"} /-->
 
-<!-- wp:navigation {"ref":50,"metadata":{"blockVisibility":{"viewport":{"desktop":false}}}} /--></div>
+<!-- wp:navigation {"metadata":{"blockVisibility":{"viewport":{"desktop":false}}}} /--></div>
 <!-- /wp:group --></div>
 <!-- /wp:group --></div>
 <!-- /wp:group -->
